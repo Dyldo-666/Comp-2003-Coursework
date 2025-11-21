2c416930f75f782af2253a7ee8109e166ae43b38
MaksymLypai <maksym.lypai@students.plymouth.ac.uk>
2025-11-21 17:18:17 +0000

    Update api.ts

--- SourceCode/app/api/lib/api.ts
+++ SourceCode/app/api/lib/api.ts
@@ -1,1 +1,10 @@
+export async function getBlogPosts() {
+  const res = await fetch("/api/blog")
+  return res.json()
+}
+
+export async function getHealth() {
+  const res = await fetch("/api/health")
+  return res.json()
+}
 
